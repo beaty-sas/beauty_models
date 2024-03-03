570e260c95a051f028b7470c1a941e7f5cbcfd59
Serhii Lakodei <serhii.lakodei@gmail.com>
2024-03-03 15:50:36 +0200

    add deletd at to offer

--- beauty_models/models/offer.py
+++ beauty_models/models/offer.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column
+from sqlalchemy import DateTime
 from sqlalchemy import DECIMAL
 from sqlalchemy import Integer
 from sqlalchemy import String
@@ -13,6 +14,7 @@ class Offer(BaseModel):
     name = Column(String(200), unique=True, nullable=False)
     price = Column(DECIMAL(precision=10, scale=2), nullable=False)  # type: ignore
     duration = Column(Integer, nullable=False)
+    deleted_at = Column(DateTime, nullable=True)
 
     businesses = relationship('Business', secondary='business_offers', back_populates='offers')
     booking = relationship('Booking', secondary='booking_offers_association', back_populates='offers')
